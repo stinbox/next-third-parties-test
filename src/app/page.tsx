dd6be914e237142c6309e41a53e1a103e878fea4
y-hiraoka <62982380+y-hiraoka@users.noreply.github.com>
2023-11-30 22:59:56 +0900

    比較用に通常iframeを置く

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,27 @@
 import { YouTubeEmbed } from "@next/third-parties/google";
-import shiki from "shiki";
-
-const code = `import { YouTubeEmbed } from "@next/third-parties/google";
-
-export default function Home() {
-  return (
-    <main>
-      <YouTubeEmbed videoid="cyFB7sB6CYs" />
-    </main>
-  );
-}
-`;
 
 export default async function Home() {
-  const highlighted = await shiki
-    .getHighlighter({ theme: "dark-plus" })
-    .then((highlighter) => highlighter.codeToHtml(code, { lang: "tsx" }));
-
   return (
-    <main className="space-y-8">
-      <div
-        className="text-sm p-2"
-        dangerouslySetInnerHTML={{ __html: highlighted }}
-      />
-      <YouTubeEmbed videoid="cyFB7sB6CYs" />
+    <main className="space-y-8 p-4">
+      <section>
+        <h1 className="text-xl mb-2 font-bold">普通の YouTube 埋め込み</h1>
+        <iframe
+          className="aspect-video"
+          width="720"
+          src="https://www.youtube.com/embed/cyFB7sB6CYs"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </section>
+      <section>
+        <h1 className="text-xl mb-2 font-bold">
+          <code>@next/third-parties</code> 経由の埋め込み
+        </h1>
+        <YouTubeEmbed
+          videoid="cyFB7sB6CYs"
+          style="background-image: url('https://i.ytimg.com/vi/cyFB7sB6CYs/maxresdefault.jpg');"
+        />
+      </section>
     </main>
   );
 }
